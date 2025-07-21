2625e74e1a20c3a2cfd022b6a7befe3aa074d43f
JohnWorkFX <johnworksfx@gmail.com>
2025-07-21 04:22:22 +0100

    Update sidenav.tsx

--- app/components/sidenav.tsx
+++ app/components/sidenav.tsx
@@ -56,14 +56,14 @@ const Sidenav: React.FC<SidenavProps> = ({ sidebarToggle, sidebarStatus }) => {
           </li>
           <hr className='py-2'/>
           <li>
-             <a href="/" className="flex items-center p-2 py-6 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+             <a href="/login" className="flex items-center p-2 py-6 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                 <LuLogIn  className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"/>
                 <span className="flex-1 ms-3 whitespace-nowrap">Login</span>
                
              </a>
           </li>
           <li>
-             <a href="/" className="flex items-center p-2 py-6 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+             <a href="/register" className="flex items-center p-2 py-6 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                 <RiUserAddLine  className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
                 <span className="flex-1 ms-3 whitespace-nowrap">Register</span>
              </a>
